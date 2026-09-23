2edd69c7cdcb7bb68847aadf3f5504b72b7a8efb
Ron <33918776+ronreezy@users.noreply.github.com>
2026-09-23 16:45:00 -0700

    Add files via upload

---
+++ portfolio/videos.js
@@ -0,0 +1,8 @@
+window.VIDEO_LINKS = {
+  "Takeoff Symphony in the Sky (music video montage)": "https://youtu.be/jkJCD4SOhkA",
+  "Free (Flash animation, rendered video)": "https://youtu.be/u8Vdovk-cwA",
+  "Wu-Tang (final video)": "https://youtu.be/mXRIGHZq81c",
+  "DED 117 framing review": "https://youtu.be/bbmKYgWsVZw",
+  "DED 117 pacing review": "https://youtu.be/g6KJlYMbirQ",
+  "DED 117 final video": "https://youtu.be/i0EGHttjI4o"
+};
